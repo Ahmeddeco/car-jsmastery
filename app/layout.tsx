1e3c5c62cce9bac327dcc26c28c8fbc855d07470
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2024-10-23 19:22:05 +0300

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata: Metadata = {
 	title: 'Car Hub',
@@ -18,6 +19,7 @@ export default function RootLayout({
 			<body className={`relative antialiased`}>
 				<Navbar />
 				{children}
+				<Analytics />
 				<Footer />
 			</body>
 		</html>
